8d6691e251a5846f6060a0e0b3dd6a3b19027df3
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-12-12 09:43:21 -0800

    Context type

---
+++ src/ContextType.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class ContexType extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (  );
+    }
+}
+
+export default ContexType;
