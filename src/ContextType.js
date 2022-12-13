89be14c4018a20f78d8e898ddf8a384c42a3b7a9
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-12-13 02:08:36 -0800

    Removed Consumer added Context type

--- src/ContextType.js
+++ src/ContextType.js
@@ -3,4 +3,3 @@ import React from 'react';
 export const MyContext = React.createContext({ Name: "Qureshi", Value: "1" });
 
 export const Provider = MyContext.Provider;
-export const Consumer = MyContext.Consumer;
--- src/Guest.jsx
+++ src/Guest.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Consumer } from "./ContextType";
-
+import { MyContext } from "./ContextType";
 class Guest extends Component {
+  static contextType = MyContext;
   render() {
     return (
       <div className="Guest">
-        <Consumer>
-          {({ data, handelClick }) => (
-            <div>
-              <h3>
-                UserName: <span>{data.name}</span>
-              </h3>
-              <h3>
-                Age: <span>{data.Age}</span>
-              </h3>
-              <button onClick={handelClick}>Change Age</button>
-            </div>
-          )}
-        </Consumer>
+        <div>
+          <h3>
+            UserName: <span>{this.context.data.name}</span>
+          </h3>
+          <h3>
+            Age: <span>{this.context.data.Age}</span>
+          </h3>
+          <button onClick={this.context.handelClick}>Change Age</button>
+        </div>
       </div>
     );
   }
