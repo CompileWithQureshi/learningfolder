a5f5938d62652c5bfccc3b170c65ae12994d5063
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-12-13 06:06:56 -0800

    Added ErorrBpund files

--- src/App.js
+++ src/App.js
@@ -7,22 +7,18 @@ import React, { Component } from 'react';
 // import EventClass from './HandelEventClass';
 // import Handeler from './HandelEvents';
 import './App.css'
-import Parent from './ParentforUserandGuest';
+// import Parent from './ParentforUserandGuest';
 // import Secound from './Secound';
 // const First = React.lazy(() => import('./First'))
 // import CodeBundel from './CodeBundle';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
+
 
   render() {
     return (<div className='App'>
       {/* <Suspense fallback={<h1 style={{ color: "red" }}>Loding....</h1>}><First /></Suspense> */}
-      <Parent />
+      {/* <Parent /> */}
       {/* <CodeBundel /> */}
       {/* <Secound /> */}
       {/* <Fragment /> */}
