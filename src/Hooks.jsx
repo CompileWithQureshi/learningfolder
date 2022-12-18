f5cea54c5e8ff852e4a38ace48d959d4abc11039
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-12-18 05:53:13 -0800

    Hooks useState

--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
+import React from "react";
 
 function Example() {
   const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>{count}Click me</button>
-    </div>
+    <>
+      Count: {count}
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+    </>
   );
 }
 
